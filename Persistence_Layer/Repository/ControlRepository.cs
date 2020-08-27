8b1cb605d73d47bd3b6288dc833d2f570d4110a0
RAJ Shakya <pawanrajshakya@gmail.com>
2020-08-27 09:58:56 -0400

    Create ControlRepository.cs

---
+++ Persistence_Layer/Repository/ControlRepository.cs
@@ -0,0 +1,19 @@
+﻿using Persistence.Interfaces;
+using Persistence.Models;
+using Persistence_Layer.Data;
+using Persistence_Layer.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repository
+{
+    public class ControlRepository: Repository<Control>, IControlRepository
+    {
+        public ControlRepository(DataContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
