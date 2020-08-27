6f531535d0344d39e1afe5c353de6e979570b8f9
RAJ Shakya <pawanrajshakya@gmail.com>
2020-08-27 09:55:53 -0400

    Create AccountController.cs

---
+++ AMS_API/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Service_Layer.Dtos;
+using Service_Layer.Interface;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : BaseApiController
+    {
+
+        public AccountController(IServiceManager service, IConfiguration config)
+            : base(service, config)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(AccountToSaveDto accountToSaveDto)
+        {
+            try
+            {
+                var accountId = await _serviceManager.Account.Add(accountToSaveDto);
+                return Ok(accountId);
+            }
+            catch (System.Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
+
+    }
+}
