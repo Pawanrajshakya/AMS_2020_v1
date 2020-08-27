c46559b493bb60a48c1424924d4ebb5243a2a8bc
RAJ Shakya <pawanrajshakya@gmail.com>
2020-08-27 09:57:26 -0400

    Create Control.cs

---
+++ Persistence_Layer/Models/Control.cs
@@ -0,0 +1,19 @@
+﻿using Persistence_Layer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Models
+{
+    public class Control
+    {
+        public int Id { get; set; }
+        public Business Business { get; set; }
+        public int BusinessId { get; set; }
+        public DateTime CurrentDate { get; set; }
+        public int AccountNoLength { get; set; }
+        public string ReportFolderName { get; set; }
+    }
+}
